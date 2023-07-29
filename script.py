8d923a4a679d93122fd58f7f818fb1c618a42699
Ayomides <>
2023-07-29 12:38:22 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,13 +1,14 @@
-import spee
+import speech_recognition as sr
 #create recogniser
 def Commandlister():
     #create the speech recogniser
     recognizer = sr.Recognizer()
-    print("listeng for speech...")
-    
-    with sr.Microphone()as source:
+    print("listening for speech...")
+    with sr.Microphone() as source:
+        recognizer.adjust_for_ambient_noise(source=source)
         audio = recognizer.listen(source)
-        query = recognizer.recognise_google(audio)
+        print("hello")
+        query = recognizer.recognize_google(audio)
         print(f'listener says: {query}')
         
 # function invocation
